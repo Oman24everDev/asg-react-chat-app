b1a51729352d63c2d165ab03f8e3f35a04eaa70f
Oman24everDev <oman24ever@gmail.com>
2023-03-22 17:28:32 +0800

    added Message component

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,6 +1,25 @@
+import Message from "./Message"
+
 const Chat = () => {
+
+  // for test purposes
+  const messages = [
+    {
+      id: 1,
+      message: "Hello there!"
+    },
+    {
+      id: 2,
+      message: "Hi,,,"
+    }
+  ]
+
   return (
-    <div>Chat</div>
+    <div className="containerWrap pb-40 pt-20">
+      {messages.map(message => (
+        <Message key={message.id} message={message}/>
+      ))}
+    </div>
   )
 }
 
---
+++ src/components/Message.jsx
@@ -0,0 +1,7 @@
+const Message = ({ message }) => {
+  return (
+    <div>{message.message}</div>
+  )
+}
+
+export default Message
