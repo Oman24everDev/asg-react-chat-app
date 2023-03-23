d6bc5502e13fc09420a0a91af68b0ddc697307cf
Oman24everDev <oman24ever@gmail.com>
2023-03-23 18:39:42 +0800

    added delete message per id

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,19 @@
 import { UserAuth } from "../context/AuthContext";
+import { doc, deleteDoc } from "firebase/firestore";
+import { async } from "@firebase/util";
+import { db } from "../database/firebase";
+
 
 const Message = ({ message }) => {
 
-    const {currentUser} = UserAuth();
+    const { currentUser } = UserAuth();
 
     console.log(message);
+
+    const handleDelete = async (id) => {
+        // console.log(id);
+        await deleteDoc(doc(db, 'messages', id))
+    }
 
     return (
         <div>
@@ -12,14 +21,16 @@ const Message = ({ message }) => {
             <div className={`chat ${message.uid === currentUser.uid ? "chat-end" : "chat-start"}`}>
                 <div className="chat-image avatar">
                     <div className="w-10 rounded-full">
-                        <img src={message.avatar}/>
+                        <img src={message.avatar} />
                     </div>
                 </div>
                 <div className="chat-header">
                     {message.name}
                 </div>
                 <div className="chat-bubble">{message.text}</div>
-
+                <div className="tooltip tooltip-error tooltip-right mt-2" data-tip="delete?">
+                    <button onClick={() => handleDelete(message.id)} className="btn btn-xs bg-white border-none text-black hover:bg-white">. . .</button>
+                </div>
             </div>
         </div>
     )
